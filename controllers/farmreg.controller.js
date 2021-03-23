dc0f505056e76e15717c3a9b2c47e6b155e3d175
anubhav14g <anubhav1408sks@gmail.com>
2021-03-23 19:52:06 +0530

    validations of farmer forms are done

--- controllers/farmreg.controller.js
+++ controllers/farmreg.controller.js
@@ -2,7 +2,74 @@ var models = require('../models');
 var exports = module.exports = {}
 
 exports.reg1p = function (req, res){
-    // console.log(req.body);
+    
+    if(typeof req.body.name!="string" && req.body.name){
+        return res.status(400).json({
+            status: "failure",
+            message: "Name must be alphabets",
+        });
+    }
+    if(typeof req.body.surname!="string" && req.body.surname){
+        return res.status(400).json({
+            status: "failure",
+            message: "Surname must be alphabets",
+        });
+    }
+    if(typeof req.body.father_or_husband_name!="string" && req.body.father_or_husband_name){
+        return res.status(400).json({
+            status: "failure",
+            message: "Father/Husband Name must be alphabets",
+        });
+    }
+    if((typeof req.body.phone!="number" || (req.body.phone).toString().length!=10) && req.body.phone){
+        return res.status(400).json({
+            status: "failure",
+            message: "Phone must be number & length must be 10",
+        });
+    }
+    if((typeof req.body.mobile!="number" || (req.body.mobile).toString().length!=10) && req.body.mobile){
+        return res.status(400).json({
+            status: "failure",
+            message: "Mobile must be number & length must be 10",
+        });
+    }
+    if((typeof req.body.whatsApp!="number" || (req.body.whatsApp).toString().length!=10) && req.body.whatsApp){
+        return res.status(400).json({
+            status: "failure",
+            message: "Whatsapp must be number & length must be 10",
+        });
+    }
+    if((typeof req.body.telegram!="number" || (req.body.telegram).toString().length!=10) && req.body.telegram){
+        return res.status(400).json({
+            status: "failure",
+            message: "Telegram must be number & length must be 10",
+        });
+    }
+    if(typeof req.body.aadharno!="number" && req.body.aadharno){
+        return res.status(400).json({
+            status: "failure",
+            message: "Aadhar No must be number",
+        });
+    }
+    if(typeof req.body.land_revenue_record_no!="number" && req.body.land_revenue_record_no){
+        return res.status(400).json({
+            status: "failure",
+            message: "Land Revenue record no must be number",
+        });
+    }
+    if(typeof req.body.survey_number_sub_survey_number!="number" && req.body.survey_number_sub_survey_number){
+        return res.status(400).json({
+            status: "failure",
+            message: "Survey no must be number",
+        });
+    }
+    if(typeof req.body.acre_hectare_goontha!="number" && req.body.acre_hectare_goontha){
+        return res.status(400).json({
+            status: "failure",
+            message: "Acre must be number",
+        });
+    }
+    
     var fpd = req.body;
     var pid = req.params.id;
     fpd.userid = pid;
@@ -68,14 +135,11 @@ exports.reg1p = function (req, res){
 }
 
 exports.reg2p = function(req, res){
-    // console.log(req.body);
+    
     var fpd = req.body;
     var pid = req.params.id;
     fpd.userid = pid;
-    // const obj={
-    //     list_of_all_records_entered_by_farmer: req.body.list_of_all_records_entered_by_farmer,
-    //     userid: pid
-    // }
+    
     models.plantg.create(fpd)
     .then(function(result){
         console.log(result);
@@ -95,22 +159,12 @@ exports.reg2p = function(req, res){
 }
 
 exports.reg3p =  function(req, res){
-    // console.log(req.body);
-    // for (var va in req.body.va) {
-    //     if (req.body.va) {
-    //       items = req.body.va;
-    //       va = JSON.stringify(items).replace(/]|[[]|"/g, '',)
-    //     }
-    //   }
+    
     var fpd = req.body;
     var pid = req.params.id;
     fpd.userid = pid;
-    // const obj={
-    //     yes_or_no: req.body.yes_or_no,
-    //     list_of_all_records_entered_by_farmer: req.body.list_of_all_records_entered_by_farmer,
-    //     userid: pid
-    // }
-      models.valueadd.create(fpd)
+
+    models.valueadd.create(fpd)
       .then(function(result){
           console.log(result);
           return res.status(200).json({
@@ -129,6 +183,26 @@ exports.reg3p =  function(req, res){
 }
 
 exports.reg4p = function(req, res){
+    
+    if(typeof req.body.name_of_organic_farm_certifying_agency!="string" && req.body.name_of_organic_farm_certifying_agency){
+        return res.status(400).json({
+            status: "failure",
+            message: "Name of organic farm must be alphabets",
+        });
+    }
+    if(typeof req.body.registration_no!="number" && req.body.registration_no){
+        return res.status(400).json({
+            status: "failure",
+            message: "Registration No must be number",
+        });
+    }
+    if(typeof req.body.have_you_done_any_lab_test!="string" && req.body.have_you_done_any_lab_test){
+        return res.status(400).json({
+            status: "failure",
+            message: "Lab test field must be string",
+        });
+    }
+
     var fpd = req.body;
     var pid = req.params.id;
     fpd.userid = pid;
@@ -151,6 +225,20 @@ exports.reg4p = function(req, res){
 }
 
 exports.reg5p = function(req, res){
+    
+    if(typeof req.body.name_of_vendor!="string" && req.body.name_of_vendor){
+        return res.status(400).json({
+            status: "failure",
+            message: "Name of Vendor must be alphabets",
+        });
+    }
+    if((typeof req.body.mobile_number!="number" || (req.body.mobile_number).toString().length!=10) && req.body.mobile_number){
+        return res.status(400).json({
+            status: "failure",
+            message: "Mobile No must be number & length must be 10",
+        });
+    }
+    
     var fpd = req.body;
     var pid = req.params.id;
     fpd.userid = pid;
@@ -240,6 +328,26 @@ exports.reg8p = function(req, res){
 }
 
 exports.reg9p = function(req, res){
+    
+    if(typeof req.body.name!="string" && req.body.name){
+        return res.status(400).json({
+            status: "failure",
+            message: "Name must be alphabets",
+        });
+    }
+    if(typeof req.body.pin_code_number!="number" && req.body.pin_code_number){
+        return res.status(400).json({
+            status: "failure",
+            message: "Pincode must be number",
+        });
+    }
+    if((typeof req.body.mobile!="number" || (req.body.mobile).toString().length!=10) && req.body.mobile){
+        return res.status(400).json({
+            status: "failure",
+            message: "Mobile must be number & length must be 10",
+        });
+    }
+
     var fpd = req.body;
     var pid = req.params.id;
     fpd.userid = pid;
@@ -262,6 +370,20 @@ exports.reg9p = function(req, res){
 }
 
 exports.reg10p = function(req, res){
+    
+    if(typeof req.body.name!="string" && req.body.name){
+        return res.status(400).json({
+            status: "failure",
+            message: "Name must be alphabets",
+        });
+    }
+    if((typeof req.body.mobile!="number" || (req.body.mobile).toString().length!=10) && req.body.mobile){
+        return res.status(400).json({
+            status: "failure",
+            message: "Mobile must be number & length must be 10",
+        });
+    }
+
     var fpd = req.body;
     var pid = req.params.id;
     fpd.userid = pid;
