ba82f3023f9be868904fe5657b1c3f7d42b5422d
anubhav14g <anubhav1408sks@gmail.com>
2021-03-24 14:01:03 +0530

    validations of buyer forms are done

--- controllers/buyreg.controller.js
+++ controllers/buyreg.controller.js
@@ -2,6 +2,50 @@ var models = require('../models');
 var exports = module.exports = {}
 
 exports.reg1p = function(req, res){
+    
+    if(typeof req.body.name!="string" && req.body.name){
+        return res.status(400).json({
+            status: "failure",
+            message: "Name must be alphabets",
+        });
+    }
+    if(typeof req.body.surname!="string" && req.body.surname){
+        return res.status(400).json({
+            status: "failure",
+            message: "Surname must be alphabets",
+        });
+    }
+    if((typeof req.body.phone!="number" || (req.body.phone).toString().length!=10) && req.body.phone){
+        return res.status(400).json({
+            status: "failure",
+            message: "Phone must be number & length must be 10",
+        });
+    }
+    if((typeof req.body.mobile!="number" || (req.body.mobile).toString().length!=10) && req.body.mobile){
+        return res.status(400).json({
+            status: "failure",
+            message: "Mobile must be number & length must be 10",
+        });
+    }
+    if((typeof req.body.whatsApp!="number" || (req.body.whatsApp).toString().length!=10) && req.body.whatsApp){
+        return res.status(400).json({
+            status: "failure",
+            message: "Whatsapp must be number & length must be 10",
+        });
+    }
+    if((typeof req.body.telegram!="number" || (req.body.telegram).toString().length!=10) && req.body.telegram){
+        return res.status(400).json({
+            status: "failure",
+            message: "Telegram must be number & length must be 10",
+        });
+    }
+    if(typeof req.body.aadhar_no!="number" && req.body.aadhar_no){
+        return res.status(400).json({
+            status: "failure",
+            message: "Aadhar No must be number",
+        });
+    }
+
     var fpd = req.body;
     var pid = req.params.id;
     fpd.userid = pid;
@@ -24,7 +68,50 @@ exports.reg1p = function(req, res){
 }
 
 exports.reg2p = function(req, res){
-    // console.log(req.body);
+    
+    if(typeof req.body.name_of_the_company_organization!="string" && req.body.name_of_the_company_organization){
+        return res.status(400).json({
+            status: "failure",
+            message: "Name of company must be alphabets",
+        });
+    }
+    if((typeof req.body.phone!="number" || (req.body.phone).toString().length!=10) && req.body.phone){
+        return res.status(400).json({
+            status: "failure",
+            message: "Phone must be number & length must be 10",
+        });
+    }
+    if((typeof req.body.mobile!="number" || (req.body.mobile).toString().length!=10) && req.body.mobile){
+        return res.status(400).json({
+            status: "failure",
+            message: "Mobile must be number & length must be 10",
+        });
+    }
+    if((typeof req.body.whatsApp!="number" || (req.body.whatsApp).toString().length!=10) && req.body.whatsApp){
+        return res.status(400).json({
+            status: "failure",
+            message: "Whatsapp must be number & length must be 10",
+        });
+    }
+    if((typeof req.body.telegram!="number" || (req.body.telegram).toString().length!=10) && req.body.telegram){
+        return res.status(400).json({
+            status: "failure",
+            message: "Telegram must be number & length must be 10",
+        });
+    }
+    if((typeof req.body.gstin_no!="number") && req.body.gstin_no){
+        return res.status(400).json({
+            status: "failure",
+            message: "GST no must be number",
+        });
+    }
+    if((typeof req.body.cin_no!="number") && req.body.cin_no){
+        return res.status(400).json({
+            status: "failure",
+            message: "CIN no must be number",
+        });
+    }
+
     var fpd = req.body;
     var pid = req.params.id;
     fpd.userid = pid;
@@ -114,6 +201,20 @@ exports.reg5p =  function(req, res){
 }
 
 exports.reg6p = function(req, res){
+    
+    if(typeof req.body.name!="string" && req.body.name){
+        return res.status(400).json({
+            status: "failure",
+            message: "Name must be alphabets",
+        });
+    }
+    if((typeof req.body.contact_no!="number" || (req.body.contact_no).toString().length!=10) && req.body.contact_no){
+        return res.status(400).json({
+            status: "failure",
+            message: "Contact No must be number & length must be 10",
+        });
+    }
+
     var fpd = req.body;
     var pid = req.params.id;
     fpd.userid = pid;
@@ -136,6 +237,20 @@ exports.reg6p = function(req, res){
 }
 
 exports.reg7p = function(req, res){
+    
+    if(typeof req.body.name!="string" && req.body.name){
+        return res.status(400).json({
+            status: "failure",
+            message: "Name must be alphabets",
+        });
+    }
+    if((typeof req.body.contact_no!="number" || (req.body.contact_no).toString().length!=10) && req.body.contact_no){
+        return res.status(400).json({
+            status: "failure",
+            message: "Contact No must be number & length must be 10",
+        });
+    }
+
     console.log(req.body);
     var fpd = req.body;
     var pid = req.params.id;
@@ -225,6 +340,26 @@ exports.reg10p = function(req, res){
 }
 
 exports.reg11p = function(req, res){
+    
+    if(typeof req.body.name!="string" && req.body.name){
+        return res.status(400).json({
+            status: "failure",
+            message: "Name must be alphabets",
+        });
+    }
+    if(typeof req.body.pin_code_number!="number" && req.body.pin_code_number){
+        return res.status(400).json({
+            status: "failure",
+            message: "Pincode No must be number",
+        });
+    }
+    if((typeof req.body.mobile!="number" || (req.body.mobile).toString().length!=10) && req.body.mobile){
+        return res.status(400).json({
+            status: "failure",
+            message: "Mobile No must be number & length must be 10",
+        });
+    }
+
     var fpd = req.body;
     var pid = req.params.id;
     fpd.userid = pid;
